1ebd4e9ae115a28680d8291d5703b273a800cace
Rakka Rage <rakkarage@gmail.com>
2019-04-11 21:32:28 -0400

    x

--- NewTestChat/Pages/Error.cshtml.cs
+++ NewTestChat/Pages/Error.cshtml.cs
@@ -7,9 +7,7 @@ namespace NewTestChat.Pages
 	public class ErrorModel : PageModel
 	{
 		public string RequestId { get; set; }
-
 		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-
 		public void OnGet()
 		{
 			RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
