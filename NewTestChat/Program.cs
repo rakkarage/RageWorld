632f84475d1ac22efed1764fcd559743aa2227d2
Rakka Rage <rakkarage@gmail.com>
2019-04-13 14:19:17 -0400

    x

--- NewTestChat/Program.cs
+++ NewTestChat/Program.cs
@@ -9,7 +9,6 @@ namespace NewTestChat
 		{
 			CreateWebHostBuilder(args).Build().Run();
 		}
-
 		public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
 			WebHost.CreateDefaultBuilder(args)
 			.UseStartup<Startup>();
